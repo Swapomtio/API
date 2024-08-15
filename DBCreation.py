e0dbc3908572a15b5dc3eba47cfaacfb4581ef61
Swapomtio <100642483+Swapomtio@users.noreply.github.com>
2024-08-15 10:36:20 -0500

    Update DBCreation.py

--- DBCreation.py
+++ DBCreation.py
@@ -11,4 +11,58 @@ conn = psycopg2.connect(
 
 conn.autocommit = True
 
+# creating a cursor object
+cursor = conn.cursor()
+
+# query to create a database
+sql = ''' CREATE database icstest ;'''
+
+# executing above query
+cursor.execute(sql)
+print("Database has been created successfully !!")
+conn.close()
+
+#
+conn = psycopg2.connect(
+	database="",
+	user="icstest",
+	password="ubuntu",
+	host="localhost",
+	port="5432"
+)
+
+conn.autocommit = True
 print("Successful connection to the database")
+# creating a cursor object
+cursor = conn.cursor()
+
+# create the table
+# all the booleans
+cur.execute("""
+CREATE TABLE subABool
+(
+ID INT PRIMARY KEY NOT NULL, 
+pointName VARCHAR,
+value BOOL
+)
+""")
+conn.commit()
+print("Table subABool created successfully")
+
+# all the real numbers
+cur.execute("""
+CREATE TABLE subAREAL
+(
+ID INT PRIMARY KEY NOT NULL,
+pointName VARCHAR,
+value REAL
+)
+""")
+conn.commit()
+print("Table subAReal created successfully")
+
+#close cursor
+cur.close()
+
+
+
