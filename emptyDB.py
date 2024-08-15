af89425ffa6535242c2d82c25779325df75b0bec
Swapomtio <100642483+Swapomtio@users.noreply.github.com>
2024-08-15 10:46:45 -0500

    Update emptyDB.py

--- emptyDB.py
+++ emptyDB.py
@@ -1,1 +1,23 @@
 #empty the db
+import psycopg2
+
+#connect
+conn = psycopg2.connect(
+	database="plctest",
+	user="postgres",
+	password="ubuntu",
+	host="localhost",
+	port="5432"
+)
+conn.autocommit = True
+# creating a cursor object
+cursor = conn.cursor()
+
+# query to drop the database
+sql = ''' DROP database icstest; '''
+
+# execute the above query
+cursor.execute(sql)
+print("Database has been deleted successfully !!")
+conn.close()
+
